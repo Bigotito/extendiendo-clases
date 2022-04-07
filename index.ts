c5f3e5aa8c72c0839c3f93f74b0b1f757fa66664
Gabriel González <ggonzalez@omixom.com>
2022-04-07 11:50:15 -0300

    ejercicio terminado

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import {readFileSync} from 'fs'
+
+type orderType = "asc" | "desc";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,59 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name:string){
+    super(name);
+    
+    JSON.parse(readFileSync(__dirname + '/products.json').toString()).forEach((item:any)=>{
+      this.addProduct(item);
+    });
+  };
+
+  addProduct(productIn:Product){
+    if(this.cosas.findIndex(function(item){
+      return item.id == productIn.id;
+    }) == -1) this.add(productIn);
+  };
+
+  getProduct(id:number){
+    return this.cosas.find((item)=>{
+      return item["id"] == id;
+    });
+  };
+
+  removeProduct(id:number){
+    const indexFinded = this.cosas.findIndex((item)=>{
+      return item["id"] == id;
+    })
+
+    if(indexFinded != -1){
+      this.cosas.splice(indexFinded,1);
+    }
+  }
+
+  getSortedByPrice(order: orderType){
+    if(order == 'asc'){
+      return this.cosas.sort((a,b)=>{
+        if(a["price"] < b["price"]){
+          return -1;
+        }
+        if(a["price"] > b["price"]){
+          return 1;
+        }
+        return 0;
+      });
+    }
+    return this.cosas.sort((a,b)=>{
+      if(a["price"] < b["price"]){
+        return 1;
+      }
+      if(a["price"] > b["price"]){
+        return -1;
+      }
+      return 0;
+    });
+  }
+}
 
 export { ListaDeProductos, Product };
